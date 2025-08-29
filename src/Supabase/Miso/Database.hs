9b961677faf2b9db8ab772cfff742bdb9386f3d2
dmjio <code@dmj.io>
2025-08-28 19:15:46 -0500

    Add more stub modules

---
+++ src/Supabase/Miso/Database.hs
@@ -0,0 +1,22 @@
+-----------------------------------------------------------------------------
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE LambdaCase                 #-}
+-----------------------------------------------------------------------------
+module Supabase.Miso.Database
+  ( -- * Functions
+    -- * Types
+  ) where
+-----------------------------------------------------------------------------
+import           Data.Hashable
+import qualified Data.HashMap.Strict as H
+import           Data.HashMap.Strict (HashMap)
+import           Data.Time
+import           Data.Aeson
+import           Control.Monad
+import           Language.Javascript.JSaddle hiding (Success)
+import           Miso hiding ((<#))
+-----------------------------------------------------------------------------
